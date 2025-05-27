c42ffefc550080bf43040f83bbc70f0ee6908827
vdv999730 <cv.desenvolvedores@gmail.com>
2025-05-27 20:35:26 -0300

    Update __init__.py

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,1 +1,12 @@
+/backend/app/
+    __init__.py
+    main.py
+    api/
+        __init__.py
+        auth.py
+        dependencies.py
+    core/
+        __init__.py
+        security.py
+    ...
 # Pasta reservada para rotas da API
