5ca4668d7b03f04097da59e5adbb785f83d47dcd
vdv999730 <cv.desenvolvedores@gmail.com>
2025-05-29 13:49:19 -0300

    teste ping

--- backend/code/api/auth.py
+++ backend/code/api/auth.py
@@ -32,3 +32,8 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
         data={"sub": user_dict["username"]}, expires_delta=access_token_expires
     )
     return {"access_token": access_token, "token_type": "bearer"}
+
+@router.get("/ping")
+def ping():
+    return {"pong": True}
+
